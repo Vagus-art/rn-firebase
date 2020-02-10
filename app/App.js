54b8dc12679c0331068d411146bea4a23b8139c6
Vagus-art <agustin.favo@gmail.com>
2020-02-09 22:04:52 -0300

    submit works

--- app/App.js
+++ app/App.js
@@ -36,17 +36,35 @@ const getCollections = async () => {
   const awaited = collection.docs.map(doc=>({id:doc.id,data:doc.data()}));
   return awaited;
 }
+
+const addItem = (first,last,born) => {
+  db.collection("users").add({
+    first,last,born
+  })
+}
+
 console.log("asddasd");
 export default function App() {
   const [collections,setCollections] = useState(null);
+  const [inputs,setInput] = useState({
+    first: '',
+    last: '',
+    born: 0
+  })
   useEffect(()=>{
     getCollections().then(data=>setCollections(data));
     console.log(collections);
   },[])
+  const onSubmit = () =>{
+    addItem(inputs.first,inputs.last,inputs.born);
+    getCollections().then(data=>setCollections(data))
+  }
   return (
     <View style={styles.container}>  
-    <Text>add asddasss sss!</Text> 
-    <Text>asda ssssd</Text>
+    <TextInput placeholder="first" onChangeText={(text)=>setInput({...inputs,first:text})} />
+    <TextInput placeholder="last" onChangeText={(text)=>setInput({...inputs,last:text})} />
+    <TextInput placeholder="born" onChangeText={(text)=>setInput({...inputs,born:text})} />
+    <Button title="submit" onPress={()=>onSubmit()} />
   {collections && collections.map(doc=><Text>{doc.id} | {doc.data.first}</Text>)}   
     </View>
   );
