b68b80bfe6601834fc4217a4fcd63ceef4a32d82
Vagus-art <agustin.favo@gmail.com>
2020-02-13 03:30:37 -0300

    replaced inputs

--- app/stack/AddUser.js
+++ app/stack/AddUser.js
@@ -3,12 +3,13 @@ import { StyleSheet, View, Button, TextInput } from "react-native";
 import { addItem } from "../firebaseActions";
 import { connect } from "react-redux";
 import { getCollections } from "../firebaseActions";
+import { Input } from "react-native-elements";
 
 const mapDispatchToProps = dispatch => ({
-    getUsers: (payload) => dispatch({type:"GET_USERS",payload}) 
-  })
+  getUsers: payload => dispatch({ type: "GET_USERS", payload })
+});
 
-const AddUser = (props) => {
+const AddUser = props => {
   const [inputs, setInput] = useState({
     first: "",
     last: "",
@@ -20,31 +21,46 @@ const AddUser = (props) => {
     props.navigation.goBack();
   };
   return (
-      <View style={styles.container}>
-        <TextInput
-          placeholder="first"
-          onChangeText={text => setInput({ ...inputs, first: text })}
-        />
-        <TextInput
-          placeholder="last"
-          onChangeText={text => setInput({ ...inputs, last: text })}
-        />
-        <TextInput
-          placeholder="born"
-          onChangeText={text => setInput({ ...inputs, born: text })}
-        />
-        <Button title="submit" onPress={() => onSubmit()} />
-      </View>
+    <View style={styles.container}>
+      <Input
+        label="First name"
+        placeholder="First"
+        leftIcon={{ name: "people" }}
+        containerStyle={{margin:20}}
+        leftIconContainerStyle={{marginLeft:5,marginRight:5}}
+        onChangeText={text => setInput({ ...inputs, first: text })}
+       
+      />
+
+      <Input
+        label="Last name"
+        placeholder="Last"
+        leftIcon={{ name: "people" }}
+        containerStyle={{margin:20}}
+        leftIconContainerStyle={{marginLeft:5,marginRight:5}}
+        onChangeText={text => setInput({ ...inputs, last: text })}
+      />
+
+      <Input
+        label="Birth year"
+        placeholder="Born"
+        leftIcon={{ name: "people" }}
+        containerStyle={{margin:20}}
+        leftIconContainerStyle={{marginLeft:5,marginRight:5}}
+        onChangeText={text => setInput({ ...inputs, born: text })}
+      />
+
+      <Button title="submit" onPress={() => onSubmit()} />
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
     alignItems: "center"
   }
 });
 
-export default connect(null,mapDispatchToProps)(AddUser);
+export default connect(null, mapDispatchToProps)(AddUser);
